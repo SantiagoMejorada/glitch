00d6b74ff605bde176f2f2bf968824720365812d
Santiago Mejorada <santiagomejorada@gmail.com>
2025-08-01 15:01:53 -0500

    feat: Instalacion de netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tu-dominio.com',
   output: 'static',
+
   build: {
     assets: 'assets'
-  }
+  },
+
+  adapter: netlify()
 });
